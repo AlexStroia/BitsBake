cafd85defb63f8a66fd178024f3fa03a16ba31f6
Alex Stroia <salexandru995@gmail.com>
2018-12-17 11:29:39 -0800

    Added app executor

---
+++ app/src/main/java/co/alexdev/bitsbake/networking/NetworkBoundResource.java
@@ -0,0 +1,4 @@
+package co.alexdev.bitsbake.networking;
+
+public abstract class NetworkBoundResource<ResultType, RequestType> {
+}
---
+++ app/src/main/java/co/alexdev/bitsbake/utils/AppExecutors.java
@@ -0,0 +1,52 @@
+package co.alexdev.bitsbake.utils;
+
+import android.os.Handler;
+import android.os.Looper;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+
+import androidx.annotation.NonNull;
+
+public class AppExecutors {
+
+    private final Object LOCK = new Object();
+
+    private final Executor diskIO;
+
+    private final Executor networkIO;
+
+    private final Executor mainThread;
+
+    private static AppExecutors sExecutor;
+
+    private AppExecutors(Executor diskIO, Executor networkIO, Executor mainThread) {
+        this.diskIO = diskIO;
+        this.networkIO = networkIO;
+        this.mainThread = mainThread;
+    }
+
+    public AppExecutors getInstance() {
+        if (sExecutor == null) {
+            synchronized (LOCK) {
+                sExecutor = new AppExecutors(Executors.newSingleThreadExecutor(),
+                        Executors.newFixedThreadPool(3),
+                        new MainThreadExecutor());
+            }
+        }
+        return sExecutor;
+    }
+
+    private static class MainThreadExecutor implements Executor {
+        private Handler mainThreadHandler = new Handler(Looper.getMainLooper());
+
+        @Override
+        public void execute(@NonNull Runnable runnable) {
+            mainThreadHandler.post(runnable);
+        }
+    }
+
+    public Executor getDiskIO() {
+        return diskIO;
+    }
+}
